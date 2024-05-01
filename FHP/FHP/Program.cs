9b9f530691a07ceb799663e6999c6464cbf9c834
Pavant0707 <pavant0707@gmail.com>
2024-05-01 09:16:26 +0530

    commit

---
+++ FHP/FHP/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHP
+{
+    class Program { 
+    
+        
+        static void Main(string[] args)
+        {
+           // String path123= @"C:\FileProg\ram.txt";
+            //File.Create(path123);
+            //String path124 = @"C:\FileProg\sai.txt";
+           // File.Move(path123,path124);
+            //Console.WriteLine("d");
+            //Console.WriteLine("A file created with name");
+            //File.WriteAllText(path123, "jai sri rama\n jai sri ram");
+            //string strings = File.ReadAllText(path123);
+            //Console.WriteLine(strings);
+            //try
+            //{
+            //    using (StreamReader reader = new StreamReader(path123))
+            //    {
+            //        int count = 0;
+            //        while (reader.ReadLine() != null)
+            //        {
+                        
+            //            count++;
+            //        }
+            //        Console.WriteLine(count);
+            //    }
+            //}catch (Exception ex)
+            //{
+                
+            //}
+
+            String[] s = { "A", "b", "C", "D" };
+            String filePath = @"C:\FileProg\array.txt";
+            try
+            {
+                using (StreamWriter stream = new StreamWriter(filePath))
+                {
+                    foreach (string line in s)
+                    {
+                        stream.WriteLine(line);
+                           
+                    }
+                }
+                Console.WriteLine("Array of string entered in file");
+
+                StreamReader streamReader = new StreamReader(filePath);
+                String contenct=streamReader.ReadToEnd();
+                Console.WriteLine(contenct[s.Length-1]);           
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+        }
+    }
+}
---
+++ FileHandling/FileHandling/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // String path1 = @"C:\FileProg\abc.txt";
+
+            //File.Create(path1);
+            //Console.WriteLine("file create");
+
+
+            //File.WriteAllText(path1, "file contain info");
+            //Console.WriteLine("inserting data");
+
+           // string content = File.ReadAllText(path1);
+           // Console.WriteLine(content);
+
+            // String path2 = @"C:\FileProg\def.text";
+
+            //// File.Copy(path, path2);
+
+            // String path3 = @"C:\FileProg\ghi.text";
+
+            // //File.Move(path2,path3);
+            // if(File.Exists(path2)) {
+            //     Console.WriteLine("fileexist");
+            // }
+            // else
+            // {
+            //     Console.WriteLine(" not file");
+            // }
+
+            //string pathnew= @"C:\FileProg\Pavan.text";
+
+            //StreamWriter streamWriter = new StreamWriter(pathnew);
+            //streamWriter.WriteLine("has some data");
+
+            //streamWriter.Close();
+
+            //StreamReader streamReader = new StreamReader(pathnew);
+            // String contenct=streamReader.ReadToEnd();
+            //Console.WriteLine( contenct);           
+
+            //string path19 = @"C:\FileProg\w.text";
+            //FileStream fileStream = new FileStream(path19, FileMode.Append, FileAccess.ReadWrite);
+            //fileStream.WriteByte(0);
+
+           
+        }
+    }
+}
